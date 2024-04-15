c87cb8a5c8b81240e002aabcc15f77dfa6a1189d
JUNTSURU <ad.free.ac.mail@gmail.com>
2024-04-16 01:35:32 +0900

    課題11

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=devide-width,initial-scale=1.0">
+    <title>Laravel課題</title>
+  </head>
+
+  <body>
+    <h1>投稿一覧</h1>
+  </body>
+</html>
